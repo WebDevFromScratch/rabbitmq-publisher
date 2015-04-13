4a80c51372f54f99f7c0b5fd64b013c1c77e85d3
PIOTR KLOSINSKI <mybettingstuff@gmail.com>
2015-04-13 13:06:18 +0200

    [#4] finish implementing Fetcher class

--- lib/fetcher.rb
+++ lib/fetcher.rb
@@ -1,8 +1,15 @@
 class Fetcher
   require 'open-uri'
 
+  attr_accessor :last_request
+
   def fetch_currencies
-    Currency.create(rates: get_rates_from_open_exchange)
+    if !self.last_request || self.last_request < Time.now - 1.hour
+      self.last_request = Time.now
+      Currency.create(rates: get_rates_from_open_exchange)
+    else
+      Currency.last
+    end
   end
 
   def get_rates_from_open_exchange
--- spec/lib/fetcher_spec.rb
+++ spec/lib/fetcher_spec.rb
@@ -8,14 +8,47 @@ RATES = '{
 }'
 
 describe Fetcher do
-  describe '#fetch_currencies' do
-    it 'works correctly' do
-      fetcher = Fetcher.new
-      allow(fetcher).to receive(:get_rates_from_open_exchange) { RATES }
-      fetcher.fetch_currencies
+  let(:fetcher) { Fetcher.new }
+  before { allow(fetcher).to receive(:get_rates_from_open_exchange) { RATES } }
 
-      expect(Currency.count).to eq(1)
-      expect(Currency.last.rates).to eq(JSON.parse(RATES))
+  describe '#fetch_currencies' do
+    before { fetcher.fetch_currencies }
+
+    it 'returns the last Currency instance' do
+      expect(fetcher.fetch_currencies).to eq(Currency.last)
+    end
+
+    context 'first request' do
+      it 'creates a new Currency instance with correct rates' do
+        expect(Currency.count).to eq(1)
+        expect(Currency.last.rates).to eq(JSON.parse(RATES))
+      end
+    end
+
+    context 'request within an hour of the previous request' do
+      before do
+        Timecop.freeze(59.minutes.from_now)
+        fetcher.fetch_currencies
+      end
+
+      after { Timecop.return }
+
+      it 'does not create a new Currency instance' do
+        expect(Currency.count).to eq(1)
+      end
+    end
+
+    context 'request after more than an hour of the previous request' do
+      before do
+        Timecop.freeze(61.minutes.from_now)
+        fetcher.fetch_currencies
+      end
+
+      after { Timecop.return }
+
+      it 'creates a new Currency instance' do
+        expect(Currency.count).to eq(2)
+      end
     end
   end
 end
