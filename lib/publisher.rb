62f9f54662925aec512177cc4e840175f8a8ce8c
PIOTR KLOSINSKI <mybettingstuff@gmail.com>
2015-04-13 16:57:28 +0200

    [#6] add Publisher class

---
+++ lib/publisher.rb
@@ -0,0 +1,21 @@
+class Publisher
+  def self.publish
+    x = channel.fanout("currencies.fanout")
+    x.publish(message)
+  end
+
+  def self.channel
+    @channel ||= connection.create_channel
+  end
+
+  def self.connection
+    @connection ||= Bunny.new.tap do |c|
+      c.start
+    end
+  end
+
+  def self.message
+    fetcher_output = Fetcher.fetch_currencies
+    { uuid: fetcher_output.uuid, rates: fetcher_output.rates['rates'] }
+  end
+end
