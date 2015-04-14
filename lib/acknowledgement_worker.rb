62339003876415fc345173241472a076fa6256e4
PIOTR KLOSINSKI <mybettingstuff@gmail.com>
2015-04-14 18:08:43 +0200

    [#7] add AcknowledgementWorker

--- lib/acknowledgement_worker.rb
+++ lib/acknowledgement_worker.rb
@@ -4,10 +4,29 @@ class AcknowledgementWorker
   from_queue "currencies.acknowledgements"
 
   def work(message)
-    consumer_id = message[:id]
-    currency = Currency.find_by(uuid: message[:uuid])
-    currency.update_attribute("consumer_#{consumer_id}", true)
+    message = JSON.parse(message)
+    consumer_id = message['id']
+    currency = Currency.find_by(uuid: message['uuid'])
 
-    ack!
+    if currency
+      currency.update_attribute("consumer_#{consumer_id}", true)
+      ack!
+    else
+      requeue_or_reject
+    end
+  end
+
+  private
+
+  def requeue_or_reject
+    if !@countdown
+      @countdown = 5
+      requeue!
+    elsif @countdown > 0
+      @countdown -= 1
+      requeue!
+    else
+      reject!
+    end
   end
 end
--- spec/lib/acknowledgement_worker_spec.rb
+++ spec/lib/acknowledgement_worker_spec.rb
@@ -2,14 +2,38 @@ require 'rails_helper'
 
 describe AcknowledgementWorker do
   describe '#work' do
-    it 'correctly updates proper `consumer_:id` attribute' do
-      currency = Currency.create
-      acknowledgement_worker = AcknowledgementWorker.new
-      message = { id: 1, uuid: currency[:uuid] }
+    let(:currency) { Currency.last }
+    let(:acknowledgement_worker) { AcknowledgementWorker.new }
+    before { Currency.create }
 
-      acknowledgement_worker.work(message)
+    context 'when a Currency is found' do
+      let(:message) { { id: 1, uuid: currency[:uuid] }.to_json }
 
-      expect(Currency.find(currency[:id])[:consumer_1]).to eq(true)
+      it 'correctly updates proper `consumer_:id` attribute' do
+        acknowledgement_worker.work(message)
+
+        expect(Currency.find(currency[:id])[:consumer_1]).to eq(true)
+      end
+    end
+
+    context 'when a Currency is not found' do
+      let(:message) { { id: 1, uuid: 'some_fake_uuid' }.to_json }
+
+      context 'and there were no more than 5 requeues' do
+        it 'returns :requeue' do
+          5.times { acknowledgement_worker.work(message) }
+
+          expect(acknowledgement_worker.work(message)).to eq(:requeue)
+        end
+      end
+
+      context 'and there were more than 5 requeues' do
+        it 'returns :reject' do
+          6.times { acknowledgement_worker.work(message) }
+
+          expect(acknowledgement_worker.work(message)).to eq(:reject)
+        end
+      end
     end
   end
 end
